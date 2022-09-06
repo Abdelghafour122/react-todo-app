3ba90d1210e74581fe6e74c1a91b16011b425e65
Abdelghafour <ghafourragg@gmail.com>
2022-09-06 19:46:15 +0100

    added firebase & tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1 className="font-[30px] font-sans ">You kept mandem waiting</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_REACT_APP_FB_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_REACT_APP_FB_PROJECTID,
+  storageBucket: process.env.REACT_APP_REACT_APP_FB_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_REACT_APP_FB_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_REACT_APP_FB_APPID,
+  //   apiKey: "AIzaSyC4Y1e63N7HwYEqvHU5V9MCd-w6EMls8jg",
+  //   authDomain: "todo-app-695d0.firebaseapp.com",
+  //   projectId: "todo-app-695d0",
+  //   storageBucket: "todo-app-695d0.appspot.com",
+  //   messagingSenderId: "1078684406745",
+  //   appId: "1:1078684406745:web:8db7f98a9acf73d48b3fc8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
