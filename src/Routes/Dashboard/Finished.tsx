ef541523bab23507a8606aded086c987b6cc2f4c
Abdelghafour <ghafourragg@gmail.com>
2022-09-22 19:35:13 +0100

    done Finished loading bug

--- src/Routes/Dashboard/Finished.tsx
+++ src/Routes/Dashboard/Finished.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import { useTodoContext } from "../../Contexts/TodoContext";
+import FinishedTodosContainer from "./Todos/FinishedTodosContainer";
+import Message from "./Todos/Message";
 
 type Props = {};
 
 const Finished = (props: Props) => {
-  return <div>Finished</div>;
+  const { todoList } = useTodoContext();
+  const [noFinishedTodos, setNoFinishedTodos] = useState<boolean>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    noFinishedTodos === undefined ? setLoading(true) : setLoading(false);
+  }, [noFinishedTodos]);
+
+  useEffect(() => {
+    const checkForUnfinishedTodos = () => {
+      return todoList.every(
+        (todo) => todo.completed === false || todo.deleted === true
+      );
+    };
+    setNoFinishedTodos(() => checkForUnfinishedTodos());
+  }, [todoList]);
+
+  return (
+    <div className="finished-todos mt-3">
+      <div className="container">
+        {loading ? (
+          <p>Loading...</p>
+        ) : noFinishedTodos === true || todoList.length === 0 ? (
+          <Message message={"Your finished todos will be moved in here!"} />
+        ) : (
+          <FinishedTodosContainer />
+        )}
+        {/* {noFinishedTodos === true || todoList.length === 0 ? (
+          <Message message={"Your finished todos will be moved in here!"} />
+        ) : (
+          <FinishedTodosContainer />
+        )} */}
+      </div>
+    </div>
+  );
 };
 
 export default Finished;
--- src/Routes/Dashboard/Trash.tsx
+++ src/Routes/Dashboard/Trash.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useTodoContext } from "../../Contexts/TodoContext";
 
 type Props = {};
 
 const Trash = (props: Props) => {
-  return <div>Trash can</div>;
+  const { todoList } = useTodoContext();
+  return (
+    <div className="trashed-todos mt-3">
+      <div className="container"></div>
+    </div>
+  );
 };
 
 export default Trash;
